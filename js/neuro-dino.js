a04bba402cc308c45d47dc463654e7bb1b9af055
v.a.antoshkin@mail.ru <v-a-antoshkin>
2022-02-19 02:13:40 +0300

    Change shadowroot

--- js/neuro-dino.js
+++ js/neuro-dino.js
@@ -58,15 +58,6 @@ function DinoFactory() {
             this.attachShadow({mode: 'open'});
 
             this.shadowRoot.append(document.importNode(dino.content, true));
-            //this.shadowRoot.insertAdjacentHTML('afterbegin', '<div></div>');
-            // `
-            //     <style>
-            //         .hidden {
-            //             display: none;
-            //         }
-            //     </style>
-            // `);
-
             this.speedX = this.maxVelocity.x;
             this.speedY = 0;
             this.dinoBrain = bestDinoBrain.clone();
