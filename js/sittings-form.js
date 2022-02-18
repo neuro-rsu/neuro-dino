a5c8f645776812e41501f608bb02da3aa52cc194
polyathlon <polyatlon.rsu.edu.ru@gmail.com>
2022-02-18 18:37:12 +0300

    PouchDB a& body-bow

---
+++ js/sittings-form.js
@@ -0,0 +1,12 @@
+function settingsFormClose() {
+    document.getElementById('sittings-form').style.display='none';
+}
+
+function settingsFormCancel() {
+    document.getElementById('sittings-form').style.display='none';
+}
+
+function settingsFormOpen() {
+    document.getElementById('sittings-form').style.display='block';
+}
+
